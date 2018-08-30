01ba727091cce54a6070afe6d4bae781c8fa4d6e
Ashish Anand <32809264+ashish1779@users.noreply.github.com>
2018-08-30 22:59:03 +0530

    Basic definition of Class, constructor and object

--- python learn.py
+++ python learn.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-"
-a = [1, 2, 3]    #List
-print(a)         #print list
-a.append(5)      #Add element in list
-print(a)         #print list with adding 5 at last
-a.pop()          #Remove last element in list
-print(a)         #print list finally
+class Robot:                                #Defining a class
+    def __init__(self,name,age):            #"__init__" is a reseved method in python classes. It is known as a constructor in object oriented concepts
+        self.name = name
+        self.age = age
+    
+    
+    def info(self):                         #Define a function named info
+        print ("My name is " + self.name)
+        print ("My age is " + self.age)
+
+r1 = Robot("Tom" , "20")                    #Creating Object r1
+r2 = Robot("Jerry" , "30")                  #Creating Object r2
+
+r1.info()                                   #See output
+r2.info()

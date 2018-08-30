49d326942ec8e70aeda3f1d865aa864b9cba1e0b
Ashish Anand <32809264+ashish1779@users.noreply.github.com>
2018-08-30 23:01:38 +0530

    Creating list & add and remove element

---
+++ List.py
@@ -0,0 +1,6 @@
+a = [1, 2, 3]    #List
+print(a)         #print list
+a.append(5)      #Add element in list
+print(a)         #print list with adding 5 at last
+a.pop()          #Remove last element in list
+print(a)         #print list finally

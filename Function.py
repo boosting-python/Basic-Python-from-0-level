70a4938f4c9f7f0c372d6206c7281f4d8c67c98a
Ashish Anand <32809264+ashish1779@users.noreply.github.com>
2018-08-31 01:17:15 +0530

    Create Function

---
+++ Function.py
@@ -0,0 +1,5 @@
+
+def product_function(x):    #Initialising a function product_function
+    return 2 * x            #Multiply argument x by 2
+a=product_function(5)       #when x=5 then a=2*5
+print(a)                    #output 10

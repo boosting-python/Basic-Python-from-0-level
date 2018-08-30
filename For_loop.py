fd425d328bd2afc70c4f1ca78bb9a6fe8930a87a
Ashish Anand <32809264+ashish1779@users.noreply.github.com>
2018-08-31 01:21:36 +0530

    Create for_loop

---
+++ For_loop.py
@@ -0,0 +1,5 @@
+for i in range(5):      #For loop i in range(5) means i is from 0(include) to 5(exclude)
+    print(i)            #Give output which is i
+    i=i+1               #every times add 1 to i till loop condition holds good
+    
+#for i in range(0,5)    #Also Same as 1st line
